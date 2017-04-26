89fcc36fb19810a17abea1b95fa4ab8ec276ce68
Nikolai R Kristiansen <nikolai@lucida.no>
2017-04-26 21:10:22 +0200

    wp++

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit0da6fe787d7ea9fdae90ba49bd73297d::getLoader();
+return ComposerAutoloaderInit6d2f5330bf06ad7dc7e00268b6bf40bf::getLoader();
